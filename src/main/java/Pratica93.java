a57016d36b038072d4be65475ba15329cfe3eae5
goettens <goettens@hotmail.com>
2015-07-20 15:17:25 -0300

    Commit2

--- src/main/java/Pratica93.java
+++ src/main/java/Pratica93.java
@@ -15,47 +15,40 @@ import utfpr.ct.dainf.if62c.pratica.*;
 
 public class Pratica93 {
     public static void main(String[] args) {
-        
         Scanner in = new Scanner(System.in);
-        List<ExecCmd> exec = new ArrayList<>();
-        String comando = "inicio";
-        int i;
-        
-        OUTER:
+        String comando;
+        List<ExecCmd> execCmd = new ArrayList<>();
+        int i = 0;
         while (true) {
             System.out.println("comando>");
-            if(in.hasNext()){
-                comando = in.nextLine();
-            }
-            if ("fim".equals(comando)) {
-                i=0;
-                for(ExecCmd a : exec){
-                    if (!a.terminado()){
+            comando = in.nextLine();
+            if (comando.equals("fim")) {
+                for (ExecCmd e : execCmd) {
+                    if (!e.terminado()) {
                         i++;
                     }
                 }
-                if (i>0) {
-                    System.out.println("Há "+ i +" processos em execução. Deseja terminá-los? (s/n) ");
-                    String terminar = null;
-                    terminar = in.next();
-                    switch (terminar) {
-                        case "s":
-                            for(ExecCmd b : exec){
-                                b.cancela();
-                                exec.remove(b);
-                                i--;
+                if (i > 0) {
+                    System.out.println("Há " +i+ " processos em execução. Deseja terminá-los? (s/n)");
+                    if (in.next().equals("s")) {
+                        for (ExecCmd c : execCmd) {
+                            if (!c.terminado()) {
+                                c.cancela();
                             }
-                            break;
-                        case "n":
-                            i=0;
-                            System.out.println("ENTROU AQUI");
-                            continue;
+                        }
+                        break;
                     }
                 }
+                if (i == 0){
+                    break;
+                }
             }
-            ExecCmd e = new ExecCmd(comando);
-            exec.add(e);
-            e.start();
+            else {
+                ExecCmd exe = new ExecCmd(comando);
+                execCmd.add(exe);
+                exe.start();
+            }
+            i = 0;
         }
     }
 }
